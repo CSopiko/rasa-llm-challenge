310aa93eaafc9b134fada8a28c6a25bcf43e91eb
CSopiko <skurd18@freeuni.edu.ge>
2023-07-19 23:20:50 +0400

    feat(utils): save mdx content

--- src/github_utils.py
+++ src/github_utils.py
@@ -17,11 +17,17 @@ def get_files_from_github_repo(owner, repo, token, tag_sha="main"):
         content = response.json()
         return content.get("tree")
     else:
-        raise ValueError(f"Error fetching {repo} contents: check {tag_sha} tag exists. response: {response.status_code}")
+        raise ValueError(
+            f"Error fetching {repo} contents: check {tag_sha} tag exists. response: {response.status_code}"
+        )
 
 
-def fetch_mdx_contents(mdx_files, wait_for_renewal=False, max_requests=60, verbose=False):
+def fetch_mdx_contents(
+    mdx_files, wait_for_renewal=False, max_requests=60, verbose=False
+):
     mdx_contents = []
+    if not wait_for_renewal:
+        return mdx_contents
     for i, file in enumerate(mdx_files):
 
         if i == max_requests and wait_for_renewal:
@@ -54,4 +60,13 @@ def load_mdx_contents(mdx_path):
 
     with open(mdx_path, "rb") as f:
         mdx_contents = pickle.load(f)
+    return mdx_contents
+
+
+def save_mdx_content(mdx_path, mdx_contents):
+    print(f"Saving mdx files to disk.")
+    import pickle
+
+    with open(mdx_path, "wb") as f:
+        pickle.dump(mdx_contents, f)
     return mdx_contents
